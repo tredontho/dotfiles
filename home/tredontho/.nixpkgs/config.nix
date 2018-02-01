1d864bdbd3e825ea60f54b196eaaeea4f52aaef0
tredontho <tredontho@gmail.com>
2018-01-31 23:47:42 -0600

    Update Vim configuration and nix-env items
    * Add vimplug support in the vimrc
    * Add nix-channels file (mostly as reference)
    * Add purescript items to nixpkgs custom config and a comment on usage

--- home/tredontho/.nixpkgs/config.nix
+++ home/tredontho/.nixpkgs/config.nix
@@ -1,17 +1,33 @@
 {
   allowUnfree = true;
   
+  # Use nix-env -iA nixpkgs.all
   packageOverrides = pkgs_ : with pkgs_; {
     all = with pkgs; buildEnv {
       name = "all";
 
       paths = [
+        # dev
+        nix-prefetch-git
+
         # haskell
         cabal2nix
-        nix-prefetch-git
         cabal-install
+
+        # purescript
+        nodejs
+        haskellPackages.purescript
+        nodePackages.pulp
+        nodePackages.grunt-cli
+        nodePackages.gulp
+        nodePackages.bower
+
         # games
         dwarf-fortress
+
+        # misc
+
+        pandoc
       ];
     };
   };
--- home/tredontho/.vimrc
+++ home/tredontho/.vimrc
@@ -1,5 +1,19 @@
 " Indentation/tab related settings
-set tabstop=4 
-set shiftwidth=4 
+set tabstop=2
+set shiftwidth=2
 set expandtab
 set autoindent
+
+" Plugins
+
+if empty(glob('~/.vim/autoload/plug.vim'))
+  silent !curl -fLo ~/.vim/autoload/plug.vim --create-dirs
+    \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+endif
+
+call plug#begin('~/.vim/vimplugs')
+
+Plug 'purescript-contrib/purescript-vim'
+
+call plug#end()
