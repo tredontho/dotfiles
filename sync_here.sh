6cbd9dc8d3a2ff1ed9707182bd5b9edb7e09b17f
tredontho <tredontho@gmail.com>
2018-04-15 01:01:03 -0500

    Fix syncing to exclude the syncing script itself, since it doesn't exist outside of this directory

--- sync_here.sh
+++ sync_here.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find . -type f -not -path "./.git/*" | sed 's/\.\//\//g' | rsync -v --files-from=- / .
+find . -type f -not -name "sync_here.sh" -not -path "./.git/*" | sed 's/\.\//\//g' | rsync -v --files-from=- / .
